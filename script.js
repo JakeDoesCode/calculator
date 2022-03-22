56472876fe14ced8c883c206e64df83082cc8cfb
Jake <classicjakeeod@gmail.com>
2022-03-22 15:39:27 -0700

    buttons return number, need to save numbers and operator and create final functions

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
 
 // taking in the inputs
-let a= prompt("num1")
-let b= prompt("num2")
-let c= prompt("oper")
+const displayOut=document.querySelector(".display-out");
+const displayIn=document.querySelector(".display-in")
 let reply_click = function(id)
  {
-     console.log(id);
+  displayIn.textContent=id;
  }
 
-let num1 = parseFloat(a);
-let op = c;
-let num2 = parseFloat(b);
+
+let num1=0;
+let tempNum1Array =[];
+let num2=0;
+let tempNum2Array= [];
+let savedValue=0
+let operator='';
+let tempOpArray=[]
+
 const squareRoot = 'squareRoot'
 
 
 //basic math functions
- function add(a,b) {
-   let sum = a+b;
-   console.log('Sum:' + sum)
-   return sum;
+ function add(num1,num2) {
+   let sum = num1+num2;
+   let roundNum= Math.round((sum + Number.EPSILON) *100) / 100;
+   roundNum=roundNum.toFixed(2);
+   console.log("Add:", roundNum)
+   
+   displayOut.textContent=roundNum;
+   
  }
  
  function sub(a,b) {
   let diff = a-b;
-  console.log('Difference:' + diff);
-  return diff;
- 
+  let roundNum= Math.round((diff+Number.EPSILON) *100)/100;
+  roundNum=roundNum.toFixed(2);
+  console.log("Add:", roundNum)
+  const displayOut=document.querySelector(".display-out");
+  displayOut.textContent=roundNum;
  }
  
  function mult(a,b){
    let product= a*b;
-   console.log('Product:'+product);
-   return product
+   let roundNum= Math.round((product+Number.EPSILON) *100)/100;
+   roundNum=roundNum.toFixed(2);
+   console.log("Add:", roundNum)
+   const displayOut=document.querySelector(".display-out");
+   displayOut.textContent=roundNum;
  }
  
  function divide(a,b){
@@ -39,22 +53,35 @@ const squareRoot = 'squareRoot'
  if(a === 0 || b === 0){ alert("One does not simply divide by zero!");
  }else{
  let quot = a/b;
- console.log('Divide:'+ quot);
- return quot;
+ let roundNum= Math.round((quot+Number.EPSILON) *100)/100;
+ roundNum=roundNum.toFixed(2);
+ console.log("Add:", roundNum);
+ const displayOut=document.querySelector(".display-out");
+ displayOut.textContent=roundNum;
    };
  }
  
  function exp(a,b){
    let pow = a**b;
-   console.log('Exponent:' + pow);
-   return pow;
+   let roundNum= Math.round((pow+Number.EPSILON) *100)/100;
+   roundNum=roundNum.toFixed(2);
+   console.log("Add:", roundNum)
+   const displayOut=document.querySelector(".display-out");
+   displayOut.textContent=roundNum;
  }
  function sqr(a){
-   if(a<=0) console.log("No.");
+  const displayOut=document.querySelector(".display-out");
+  if(a===0){
+    displayOut.textContent="Not doing that.";
+  }else{
    let sqr = Math.sqrt(a);
-   console.log(`Square root:` + sqr);
+   let roundNum= Math.round((sqr+Number.EPSILON) *100)/100;
+   roundNum=roundNum.toFixed(2);
+   console.log("Add:", roundNum)
+   displayOut.textContent=roundNum;
    return sqr;
- }
+  }
+  }
 
  
  function operate(a,b,op){
@@ -76,7 +103,7 @@ const squareRoot = 'squareRoot'
     console.log(answer);
   }
   else if (op===squareRoot){
-    let answer=sqr(a,b);
+    let answer=sqr(a);
     console.log(answer);
   }else {
     alert("Not a valid operator")
@@ -84,7 +111,7 @@ const squareRoot = 'squareRoot'
 
  }
 
+const clearButton = document.querySelector('#btnClear')
+clearButton.addEventListener('click',() => {location.reload();})
 
-operate(num1,num2,op)
- 
 
